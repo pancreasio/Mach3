d668531dfc9fa784b126366903576b6e8c8af6b5
pancreasio <julianholisu@gmail.com>
2019-09-02 10:15:52 -0300

    Grid Model created

---
+++ Gioulette Mach 3/Assets/Scripts/GridModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    public enum ChipType {R,G,B,P};
+
+    private ChipType [,]chipInstances = new ChipType [7,7];
+
+    public void SetChip(ChipType newChip, int x, int y)
+    {
+        chipInstances[x, y] = newChip;
+    }
+
+    public ChipType GetChip(int x, int y)
+    {
+        return chipInstances[x, y];
+    }
+}
