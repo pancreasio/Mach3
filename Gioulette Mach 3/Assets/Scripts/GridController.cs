8ad529881245f75868f674d254337fd851b2075f
pancreasio <julianholisu@gmail.com>
2019-09-02 12:16:51 -0300

    grid generated by code

--- Gioulette Mach 3/Assets/Scripts/GridController.cs
+++ Gioulette Mach 3/Assets/Scripts/GridController.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class GridController : MonoBehaviour
 {
+    public int width, height;
     private Grid grid;
     private GridView view;
+    public bool xd;
 
     private void Start()
     {
+        grid = new Grid();
         view = GameObject.Find("GridView").GetComponent<GridView>();
+        xd = true;
+        GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {
+        grid.NewGrid(width, height);
+        for (int i = 0; i < width; i++)
+        {
+            for (int i2 = 0; i2 < height; i2++)
+            {
+                grid.SetChip((Grid.ChipType)Random.Range((int)0, (int)4),i,i2);
+            }
+        }
     }
 
     private void Update()
     {
-        
+        if (xd)
+        {
+            view.Initialize(width, height);
+            view.DrawGrid(grid);
+            xd = false;
+        }
     }
 }
--- Gioulette Mach 3/Assets/Scripts/GridView.cs
+++ Gioulette Mach 3/Assets/Scripts/GridView.cs
@@ -1,8 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridView : MonoBehaviour
 {
-    
+    public GameObject redPrefab, greenPrefab, bluePrefab, pinkPrefab;
+    private int width, height;
+    private GameObject[,] goArray;
+
+    public void Initialize(int x, int y)
+    {
+        goArray = new GameObject[x, y];
+        width = x;
+        height = y;
+    }
+
+    public void DrawGrid(Grid grid)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int i2 = 0; i2 < height; i2++)
+            {
+                DrawChip(grid.GetChip(i, i2), i, i2);
+                Debug.Log("drawing chip: " + i + " " + i2);
+            }
+        }
+    }
+
+    private void DrawChip(Grid.ChipType chip, int x, int y)
+    {
+        float newX = new float();
+        float newY = new float();
+        newX = -2.0f + x * 0.571f;
+        newY = -4.2f + y * 0.97f;
+        switch (chip)
+        {
+            case Grid.ChipType.R:
+                Instantiate(redPrefab, new Vector2(newX, newY), Quaternion.identity);
+                break;
+            case Grid.ChipType.G:
+                Instantiate(greenPrefab, new Vector2(newX, newY), Quaternion.identity);
+                break;
+            case Grid.ChipType.B:
+                Instantiate(bluePrefab, new Vector2(newX, newY), Quaternion.identity);
+                break;
+            case Grid.ChipType.P:
+                Instantiate(pinkPrefab, new Vector2(newX, newY), Quaternion.identity);
+                break;
+            default:
+                break;
+        }
+    }
 }
