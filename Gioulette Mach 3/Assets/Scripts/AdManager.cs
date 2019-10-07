3464ae4cb9f50b820ea130302c8dd4e4cf5545f5
pancreasio <julianholisu@gmail.com>
2019-10-07 13:10:22 -0300

    ads added

---
+++ Gioulette Mach 3/Assets/Scripts/AdManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using System;
+
+
+public class AdManager : MonoBehaviour
+{
+    private const string androidID = "3317305";
+
+    private void Awake()
+    {
+        Advertisement.Initialize(androidID, true);
+    }
+
+    public void WatchVideoAd(ShowResult result)
+    {
+        Advertisement.Show("video");
+        switch (result)
+        {
+            case ShowResult.Failed:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void WatchRewardedVideoAd()
+    {
+        Advertisement.Show("rewardedVideo");
+    }
+}
